00079079005ea72a99b1d3f3007ec5f863ee0421
shennemann <s.hennemann@rto.de>
2015-11-06 16:56:43 +0100

    Improve litebox-theme (items now vertical align middle)

--- trunk/litebox-templates/default.php
+++ trunk/litebox-templates/default.php
@@ -1,11 +1,7 @@
 
 <div id="litebox-gallery" class="litebox-gallery">
-	<div class="litebox-table">
-		<div class="litebox-cell">
-			<div id="litebox-owlslider" class="litebox-owlslider owl-carousel owl-theme">
+	<div id="litebox-owlslider" class="litebox-owlslider owl-carousel owl-theme">
 
-			</div>
-		</div>
 	</div>
 	<div class="close-button"></div>
 </div>
